c5fa64ab92e91859b1629d5e30cd66a2d6f93ca0
Валентина Окулова <valya.okulova@mail.ru>
2020-03-27 13:04:43 +0700

    added pushMessage and sendMessage methods

--- src/store.js
+++ src/store.js
@@ -1,9 +1,44 @@
 import { createStore } from "redux"
 
+// ACTIONS //
+const URL = "http://localhost:8081"
+
+export function updateMessages(messages) {
+    return {
+        type: UPDATE_MESSAGES,
+        messages
+    }
+}
+
+export function getMessages() {
+    return (dispatch) => {
+        return fetch(URL)
+          .then(responce => responce.json())
+          .then((responce) => {
+            dispatch(updateMessages(responce))
+          })
+    }
+}
+
+export function pushMessage(message) {
+    return {
+        type: ADD_MESSAGE,
+        message
+    }
+}
+
+export function sendMessage(data) {
+    return (dispatch) => {
+        return fetch(URL, { method: "POST", body: JSON.stringify(data)})
+          .then((responce) => dispatch(pushMessage(data)))
+    }
+}
+
+// STORE //
+
 const initialState = {
     messages: []
 }
-
 export default createStore(function (state = initialState, action){
     switch(action.type){
         case ADD_MESSAGE: {
